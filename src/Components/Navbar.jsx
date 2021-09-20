3377a42a29ef6251d4e2eb9d73ef22c6c276aef7
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-09-20 15:44:18 +0530

    nav bar styles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,16 +4,19 @@ import styled from 'styled-components';
 
 const Container =styled.div`
     height: 60px;
-    background-color:gray;
+    
 `
 const Wrapper =styled.div`
     padding:10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 
 `;
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
  `;
  const Language = styled.span`
     font-size:14px;
@@ -21,10 +24,21 @@ const Left = styled.div`
 
  `;
  const SearchContainer = styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+ `;
+ const Input = styled.input`
+    border: none;
+ 
  `;
 const Center = styled.div`
     flex: 1;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
 `;
 const Right = styled.div`
     flex: 1;
@@ -37,13 +51,15 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        input
+                      <Input/>
                      <Search/>   
-                    
-
                     </SearchContainer>
                 </Left>
-                <Center>center</Center>
+                <Center>
+                    <Logo>
+                        LA . Deporte
+                    </Logo>
+                </Center>
                 <Right>right</Right>
             </Wrapper>
 
