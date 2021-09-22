9bc649953954d1771c8f9fc85f8d29bfd77d07ca
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-09-22 15:54:41 +0530

    finished the nav bar basic compoents

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,6 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search,ShoppingCartOutlined } from '@material-ui/icons';
+
 import React from 'react';
 import styled from 'styled-components';
 
@@ -36,12 +38,22 @@ const Left = styled.div`
  `;
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `;
 const Logo = styled.h1`
     font-weight: bold;
 `;
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content:flex-end;
+`;
+const MenuItem = styled.div`
+    font-size:14px; 
+    cursor: pointer;
+    margin-left: 25px;
+
 `;
 
 const Navbar = () => {
@@ -52,7 +64,7 @@ const Navbar = () => {
                     <Language>EN</Language>
                     <SearchContainer>
                       <Input/>
-                     <Search/>   
+                     <Search style={{color:"gray", fontSize:16}}/>   
                     </SearchContainer>
                 </Left>
                 <Center>
@@ -60,7 +72,15 @@ const Navbar = () => {
                         LA . Deporte
                     </Logo>
                 </Center>
-                <Right>right</Right>
+                <Right>
+                    <MenuItem>REGISTER </MenuItem>
+                    <MenuItem>SIGN IN </MenuItem>
+                    <MenuItem>
+                       <Badge badgeContent={4} color="primary" >
+                           <ShoppingCartOutlined/>
+                       </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
 
         </Container>
