92d8886f78c82050a553f412d600ae1a18f8f0a5
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-09-22 16:18:12 +0530

    created slider compoent

--- src/Components/Announcement.jsx
+++ src/Components/Announcement.jsx
@@ -8,6 +8,11 @@ const Container = styled.div`
     height: 30px;
     background-color:teal;
     color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight:500;
 `;
 
 
@@ -16,7 +21,7 @@ const Container = styled.div`
 const Announcement = () => {
     return (
        <Container>
-           Supper Deals !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+           THE 21/22 THIRD KIT IS HERE - SHOP NOW !
        </Container>
     )
 }
