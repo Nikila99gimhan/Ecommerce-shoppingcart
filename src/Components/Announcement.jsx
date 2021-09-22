ab1140117c178717ea95c7c2d748ff45255376fa
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-09-22 16:08:27 +0530

    Added annuncement section above navbar

---
+++ src/Components/Announcement.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+
+
+
+
+const Container = styled.div`
+    height: 30px;
+    background-color:teal;
+    color: white;
+`;
+
+
+
+
+const Announcement = () => {
+    return (
+       <Container>
+           Supper Deals !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+       </Container>
+    )
+}
+
+export default Announcement
